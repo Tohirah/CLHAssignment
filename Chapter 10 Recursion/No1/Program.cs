5e62405693df214ad09ebe90c668545e2050dbe1
LOLA-BANKS <you@example.com>
2022-08-03 23:23:04 +0100

    Recursion

---
+++ Chapter 10 Recursion/No1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace No1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter the nth term");
+            int n = int.Parse(Console.ReadLine());
+
+            int k = n;
+            NestedLoops(n, k);
+        }
+        static void NestedLoops(int index, int row)
+        {
+                if(row == 0)
+                {
+                    return;
+                }
+
+                NestedLoops (index, row - 1);
+
+                for(int m = 1; m <= row; m++)
+                {
+                    Console.Write(m + " ");
+                }
+                Console.WriteLine("");
+                
+               
+        }
+    }
+}
---
+++ Chapter 10 Recursion/No2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace No2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter desired number");
+            int argPrint = int.Parse(Console.ReadLine());
+
+            Print(argPrint, argPrint);
+        }
+        public static void Print(int v, int n)
+                { 
+                    if(v > 1)
+                        {
+                        for(int i = 1; i <= n; i++)
+                            {
+                                Console.Write(n-(v-1) + " " + i + "  ");
+                            }
+                            Console.WriteLine(" ");
+                        }
+                        if(v == 1)
+                        {
+                        for(int i = 1; i <= n; i++)
+                            {
+                                Console.Write(n-(v-1) + " " + i + "  ");
+                            }
+                            return;
+                        }
+                    Print(v - 1, n);
+                }
+    }
+}
